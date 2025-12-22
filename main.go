9336735ef29b5f4d14f1abf2f2afc7775c785b17
ThakdanaiDL <thakdanai.limthongkum@gmail.com>
2025-12-22 14:25:51 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Health Check Route
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"status":  "UP",
+			"message": "Echo API is running on Cloud Run!",
+		})
+	})
+
+	// สำคัญ: Cloud Run จะกำหนด Port ผ่าน Environment Variable ชื่อ PORT
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
